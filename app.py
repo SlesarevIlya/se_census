530d335956cf8185555bab969807fc630d172672
ilia.slesarev <ilia.slesarev@semrush.com>
2022-09-18 22:03:58 +0300

    Created basic communication entity

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import logging
 from typing import NoReturn
 
-from telegram.ext import (Application, CommandHandler,
-                          InlineQueryHandler)
+from telegram.ext import Application, CommandHandler, InlineQueryHandler
 
-from bot.basic_communication import help, inline_query, my_profile, start
+from bot.basic_communication import BasicCommunication
 from bot.credentials import bot_token, postgre_creds
 from bot.postgre.users import PgUsers
 from bot.who_am_i import WhoAmIConversation
@@ -16,13 +15,18 @@ logger = logging.getLogger(__name__)
 def main() -> NoReturn:
     application = Application.builder().token(bot_token).build()
 
-    # application.add_handler(ChatMemberHandler(track_chats, ChatMemberHandler.MY_CHAT_MEMBER))
+    pg_client: PgUsers = PgUsers(pg_creds=postgre_creds)
 
-    application.add_handler(WhoAmIConversation(pg_conn=PgUsers(pg_creds=postgre_creds), logger=logger).get_handler())
-    application.add_handler(CommandHandler("myprofile", my_profile))
-    application.add_handler(InlineQueryHandler(inline_query))
-    application.add_handler(CommandHandler('start', start))
-    application.add_handler(CommandHandler('help', help))
+    basic_comm: BasicCommunication = BasicCommunication(pg_conn=pg_client,
+                                                        logger=logger)
+    who_am_i_conv: WhoAmIConversation = WhoAmIConversation(pg_conn=pg_client,
+                                                           logger=logger)
+
+    application.add_handler(who_am_i_conv.get_handler())
+    application.add_handler(CommandHandler("myprofile", basic_comm.my_profile))
+    application.add_handler(InlineQueryHandler(basic_comm.inline_query))
+    application.add_handler(CommandHandler('start', basic_comm.start))
+    application.add_handler(CommandHandler('help', basic_comm.help))
 
     application.run_polling()
 
--- bot/basic_communication.py
+++ bot/basic_communication.py
@@ -8,39 +8,36 @@ from telegram.ext import CallbackContext, ContextTypes
 from bot.credentials import postgre_creds
 from bot.postgre.users import PgUsers
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
 
-logger = logging.getLogger(__name__)
+class BasicCommunication:
+    def __init__(self, pg_conn, logger: logging.Logger):
+        self.pg_conn = pg_conn
+        self.logger = logger
 
+    async def start(self, update: Update, context: CallbackContext):
+        await update.message.reply_text("Hi SE participant!")
 
-async def start(update: Update, context: CallbackContext):
-    await update.message.reply_text("Hi SE participant!")
+    async def help(self, update: Update, context: CallbackContext):
+        await update.message.reply_text("WTF am I doing?? Help yourself!")
 
+    async def my_profile(self, update: Update, context: CallbackContext):
+        pg_conn = PgUsers(pg_creds=postgre_creds)
+        user_meta = [user.full_meta() for user in pg_conn.get_users_by_name(name=update.message.from_user.name,
+                                                                            substring=False)]
+        await update.message.reply_text(text=user_meta[0])
 
-async def help(update: Update, context: CallbackContext):
-    await update.message.reply_text("WTF am I doing?? Help yourself!")
+    async def inline_query(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> NoReturn:
+        query = update.inline_query.query
 
+        if query == "":
+            return
 
-async def my_profile(update: Update, context: CallbackContext):
-    pg_conn = PgUsers(pg_creds=postgre_creds)
-    user_meta = [user.full_meta() for user in pg_conn.get_users_by_name(name=update.message.from_user.name,
-                                                                        substring=False)]
-    await update.message.reply_text(text=user_meta[0])
+        pg_conn = PgUsers(pg_creds=postgre_creds)
+        results = [InlineQueryResultArticle(id=str(uuid4()),
+                                            title=user.full_name(),
+                                            input_message_content=InputTextMessageContent(user.full_meta()),
+                                            description=user.description())
+                   for user in pg_conn.get_users_by_name(name=query,
+                                                         substring=True)]
 
-
-async def inline_query(update: Update, context: ContextTypes.DEFAULT_TYPE) -> NoReturn:
-    query = update.inline_query.query
-
-    if query == "":
-        return
-
-    pg_conn = PgUsers(pg_creds=postgre_creds)
-    results = [InlineQueryResultArticle(id=str(uuid4()),
-                                        title=user.full_name(),
-                                        input_message_content=InputTextMessageContent(user.full_meta()),
-                                        description=user.description())
-               for user in pg_conn.get_users_by_name(name=query,
-                                                     substring=True)]
-
-    await update.inline_query.answer(results)
+        await update.inline_query.answer(results)
